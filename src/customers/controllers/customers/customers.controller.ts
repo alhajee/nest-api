b820a350c2787523d0af05d3c109ace88a081e69
Muhammad Alhajee <alhajee2009@gmail.com>
2023-04-09 11:24:37 +0100

    feat: return dummy customer

--- src/customers/controllers/customers/customers.controller.ts
+++ src/customers/controllers/customers/customers.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 
 @Controller('customers')
-export class CustomersController {}
+export class CustomersController {
+  @Get('')
+  getCustomer() {
+    return {
+      id: 1,
+      email: 'dummy@email.com',
+      createdAt: new Date(),
+    };
+  }
+}
