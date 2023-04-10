af9c9aa929552e022e5dd9c32b64f8f9c51d55f3
Muhammad Alhajee <alhajee2009@gmail.com>
2023-04-10 23:39:40 +0100

    feat: implement authorization middleware

--- src/customers/customers.module.ts
+++ src/customers/customers.module.ts
@@ -1,9 +1,25 @@
-import { Module } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 import { CustomersController } from './controllers/customers/customers.controller';
 import { CustomersService } from './services/customers/customers.service';
+import { ValidateCustomerMiddleware } from './middlewares/validate-customer/validate-customer.middleware';
 
 @Module({
   controllers: [CustomersController],
-  providers: [CustomersService]
+  providers: [CustomersService],
 })
-export class CustomersModule {}
+export class CustomersModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(ValidateCustomerMiddleware)
+      .exclude({
+        path: 'customers/search/:customerId',
+        method: RequestMethod.GET,
+      })
+      .forRoutes(CustomersController);
+  }
+}
--- src/customers/middlewares/validate-customer/validate-customer.middleware.ts
+++ src/customers/middlewares/validate-customer/validate-customer.middleware.ts
@@ -1,9 +1,20 @@
-import { Injectable, NestMiddleware } from '@nestjs/common';
+import { HttpStatus, Injectable, NestMiddleware } from '@nestjs/common';
 
 @Injectable()
 export class ValidateCustomerMiddleware implements NestMiddleware {
   use(req: any, res: any, next: () => void) {
     console.log('Hello world! ValidateCustomer Middleware triggered!');
+    const { authorization } = req.headers;
+    if (!authorization) {
+      return res.status(HttpStatus.FORBIDDEN).send({
+        error: 'No Authentication Token provided',
+      });
+    }
+    if (authorization !== '1234') {
+      return res.status(HttpStatus.FORBIDDEN).send({
+        error: 'Invalid Authorization Token provided',
+      });
+    }
     next();
   }
 }
