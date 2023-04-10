f72a42446c8bc294d48d44baedabbc2766c3f34f
Muhammad Alhajee <alhajee2009@gmail.com>
2023-04-10 23:41:51 +0100

    refactor: type anotate middleware next function

--- src/customers/middlewares/validate-customer/validate-customer.middleware.ts
+++ src/customers/middlewares/validate-customer/validate-customer.middleware.ts
@@ -1,8 +1,9 @@
 import { HttpStatus, Injectable, NestMiddleware } from '@nestjs/common';
+import { NextFunction } from 'express';
 
 @Injectable()
 export class ValidateCustomerMiddleware implements NestMiddleware {
-  use(req: any, res: any, next: () => void) {
+  use(req: any, res: any, next: NextFunction) {
     console.log('Hello world! ValidateCustomer Middleware triggered!');
     const { authorization } = req.headers;
     if (!authorization) {
