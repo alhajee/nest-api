e7b74642e5f4f6f5aecd6b9cf779a111f0986f8b
Muhammad Alhajee <alhajee2009@gmail.com>
2023-04-10 15:33:40 +0100

    feat: users controller serialize partial user

--- src/users/controllers/users/users.controller.ts
+++ src/users/controllers/users/users.controller.ts
@@ -1,5 +1,15 @@
-import { Controller, Get, Inject, Param } from '@nestjs/common';
+import {
+  ClassSerializerInterceptor,
+  Controller,
+  Get,
+  HttpException,
+  HttpStatus,
+  Inject,
+  Param,
+  UseInterceptors,
+} from '@nestjs/common';
 import { UsersService } from 'src/users/services/users/users.service';
+import { SerializedUser } from 'src/users/types';
 
 @Controller('users')
 export class UsersController {
@@ -12,8 +22,14 @@ export class UsersController {
     return this.userService.getUsers();
   }
 
+  @UseInterceptors(ClassSerializerInterceptor)
   @Get('/:username')
   getByUsername(@Param('username') username: string) {
-    return this.userService.getUserByUsername(username);
+    const user = this.userService.getUserByUsername(username);
+    if (user) {
+      return new SerializedUser(user);
+    } else {
+      throw new HttpException('User not found!', HttpStatus.BAD_REQUEST);
+    }
   }
 }
