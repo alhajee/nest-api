78f5864c8aae86eb93c79ca0d7bd5a5b1d63de49
Muhammad Alhajee <alhajee2009@gmail.com>
2023-04-11 09:39:52 +0100

    refactor: app module import postgres database module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { CustomersModule } from './customers/customers.module';
 import { UsersModule } from './users/users.module';
+import { ConfigModule } from '@nestjs/config';
+import { DatabaseModule } from './database.module';
 
 @Module({
-  imports: [CustomersModule, UsersModule],
+  imports: [
+    CustomersModule,
+    UsersModule,
+    ConfigModule.forRoot({
+      envFilePath: ['docker.env', '.env'],
+    }),
+    DatabaseModule,
+  ],
   controllers: [],
   providers: [],
 })
