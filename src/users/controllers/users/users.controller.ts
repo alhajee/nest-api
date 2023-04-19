0fa159f35cd703d120ba7676233d8080b5377a27
Muhammad Alhajee <alhajee2009@gmail.com>
2023-04-20 00:30:53 +0100

    refactor: user controller update user by id

--- src/users/controllers/users/users.controller.ts
+++ src/users/controllers/users/users.controller.ts
@@ -1,12 +1,19 @@
 import {
+  Body,
   ClassSerializerInterceptor,
   Controller,
   Get,
   Inject,
   Param,
+  ParseUUIDPipe,
+  Post,
+  Put,
   UseFilters,
   UseInterceptors,
+  ValidationPipe,
 } from '@nestjs/common';
+import { CreateUserDto } from 'src/users/dtos/CreateUser.dto';
+import { UpdateUserDto } from 'src/users/dtos/UpdateUser.dto';
 import { UserNotFoundException } from 'src/users/exceptions/UserNotFound.exception';
 import { HttpExceptionFilter } from 'src/users/filters/HttpException.filter';
 import { UsersService } from 'src/users/services/users/users.service';
@@ -20,19 +27,42 @@ export class UsersController {
 
   @UseInterceptors(ClassSerializerInterceptor)
   @Get('')
-  getUsers() {
-    return this.userService.getUsers();
+  async getUsers() {
+    return this.userService.findUsers();
+  }
+
+  @Post('')
+  createUser(@Body() createUserDto: CreateUserDto) {
+    return this.userService.createUser(createUserDto);
+  }
+
+  @Get(':id')
+  async getUser(@Param('id', ParseUUIDPipe) id: string) {
+    const user = await this.userService.findUser(id);
+    if (user) {
+      return user;
+    } else {
+      throw new UserNotFoundException();
+    }
   }
 
   @UseInterceptors(ClassSerializerInterceptor)
   @UseFilters(HttpExceptionFilter)
   @Get('/:username')
-  getByUsername(@Param('username') username: string) {
-    const user = this.userService.getUserByUsername(username);
+  async getByUsername(@Param('username') username: string) {
+    const user = await this.userService.findUserByUsername(username);
     if (user) {
-      return new SerializedUser(user);
+      return user;
     } else {
       throw new UserNotFoundException();
     }
+  }
+
+  @Put(':id')
+  updateUserById(
+    @Param('id', ParseUUIDPipe) id: string,
+    @Body() updateUserDto: UpdateUserDto,
+  ) {
+    return this.userService.updateUser(id, updateUserDto);
   }
 }
