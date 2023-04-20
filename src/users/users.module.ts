ad286ee11d40bfdab3e11ec9a37e15e5c8d6e77b
Muhammad Alhajee <alhajee2009@gmail.com>
2023-04-20 13:47:45 +0100

    refactor: user module import profile entity as dependency

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,9 +3,10 @@ import { UsersController } from './controllers/users/users.controller';
 import { UsersService } from './services/users/users.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
+import { Profile } from './entities/profile.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User, Profile])],
   controllers: [UsersController],
   providers: [
     {
